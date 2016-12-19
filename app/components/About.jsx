22a79b22c587dd88e232ced7115a972f618cece9
qory user <maulsby.mo@gmail.com>
2016-12-18 20:10:23 -0600

    Update About Page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,13 @@
 var React = require("react");
+var {Link} = require("react-router");
 
 var About = (props) => {
     return (
       <div>
-        <h3>About </h3>
-        <p>Welcome to the About Page</p>
+        <h1 className="text-center"> About</h1>
+        <p>Welcome to the React Weather App, where you can find out the weather in any city.</p>
+        <h4>API used</h4>
+        <p>In this app we have used the <a href="http://www.openweathermap.org/" target="_blank" >OpenWeatherMap</a> API</p>
       </div>
 
     );
