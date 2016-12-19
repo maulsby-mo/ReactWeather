87f3513ddfbcba1c1652b99dbbc54b73bc7eacd6
qory user <maulsby.mo@gmail.com>
2016-12-18 22:24:08 -0600

    Switch to scss

--- app/app.jsx
+++ app/app.jsx
@@ -9,7 +9,7 @@ var Examples = require('Examples');
 //load foundation
 require('style!css!foundation-sites/dist/css/foundation.min.css');
 //app css
-require('style!css!applicationStyles')
+require('style!css!sass!applicationStyles')
 $(document).foundation();
 
 ReactDOM.render(
